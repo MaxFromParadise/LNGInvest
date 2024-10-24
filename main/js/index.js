85f76ddbd29e78bd09fd135181d900af23279992
Maxim Polanov <maxdemented372@gmail.com>
2024-10-24 10:17:33 +0300

    main page fix navbar

--- main/js/index.js
+++ main/js/index.js
@@ -6,11 +6,24 @@ const menu = document.querySelector('.menu');
 const overlay = document.querySelector('.overlay');
 const body = document.body;
 
+// Function to get scrollbar width
+function getScrollbarWidth() {
+	return window.innerWidth - document.documentElement.clientWidth;
+}
+
 // Burger menu toggle functions
 function toggleMenu() {
 	const isOpen = menu.classList.toggle('open');
 	burger.classList.toggle('open');
 	overlay.style.display = isOpen ? 'flex' : 'none';
+
+	if (isOpen) {
+		const scrollbarWidth = getScrollbarWidth();
+		body.style.paddingRight = `${scrollbarWidth}px`;
+	} else {
+		body.style.paddingRight = '';
+	}
+
 	body.classList.toggle('lock', isOpen);
 }
 
@@ -18,6 +31,7 @@ function closeMenu() {
 	burger.classList.remove('open');
 	menu.classList.remove('open');
 	overlay.style.display = 'none';
+	body.style.paddingRight = '';
 	body.classList.remove('lock');
 }
 
