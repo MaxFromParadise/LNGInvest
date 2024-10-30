7c5640572126b65e58670a6e7e33bcb5e90a3325
Maxim Polanov <maxdemented372@gmail.com>
2024-10-30 20:40:26 +0300

    parallax on projects-test2

--- projects-test2/js/index.js
+++ projects-test2/js/index.js
@@ -31,17 +31,48 @@ function closeMenu() {
 burger.addEventListener('click', toggleMenu);
 overlay.addEventListener('click', closeMenu);
 
-document.querySelectorAll('.categories-sticky__button').forEach((button) => {
+document.querySelectorAll('.categories-project__button').forEach((button) => {
 	button.addEventListener('click', function () {
-		// Добавляем класс active к кнопке
 		this.classList.toggle('active');
 
-		// Находим соседний элемент с классом categories-sticky__items
-		const sibling = this.closest('.categories-sticky').querySelector(
-			'.categories-sticky__items'
+		const sibling = this.closest('.categories-project').querySelector(
+			'.categories-project__items'
 		);
 		if (sibling) {
 			sibling.classList.toggle('active');
 		}
 	});
+});
+//
+document.addEventListener('scroll', function () {
+	const sections = document.querySelectorAll('.project');
+	let activeSection = null;
+
+	sections.forEach((section) => {
+		const rect = section.getBoundingClientRect();
+
+		if (
+			rect.top <= window.innerHeight / 1 &&
+			rect.bottom >= window.innerHeight / 1
+		) {
+			activeSection = section;
+			section.classList.add('active');
+		} else {
+			section.classList.remove('active');
+		}
+	});
+
+	if (activeSection) {
+		const sectionHeight = activeSection.offsetHeight;
+		const scrollPosition = window.scrollY - activeSection.offsetTop;
+		const scrollRatio = scrollPosition / sectionHeight;
+
+		const bg = activeSection.querySelector('.project__bg');
+		const content = activeSection.querySelector('.project__content');
+		const image = activeSection.querySelector('.project__image');
+
+		bg.style.transform = `translateY(-${scrollRatio * 100}px)`;
+		content.style.transform = `translateY(-${scrollRatio * 200}px)`;
+		image.style.transform = `translateY(-${scrollRatio * 300}px)`;
+	}
 });
